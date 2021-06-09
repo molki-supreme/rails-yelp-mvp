7654f971ed4ba1963cdf7893a28156a3a2d4db24
MarieCichon <marie-chn@hotmail.fr>
2021-06-09 16:53:52 +0200

    donne

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: (0..5).to_a }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts 'clean db'
+Restaurant.destroy_all
+puts 'create seed db'
+
+5.times do
+  Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.full_address,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+puts 'done'
